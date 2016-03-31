69381b5e6dd480392f4597acb19d7b2e01f897fd
Marcello Lins <marcello.grechi@gmail.com>
2016-03-31 09:55:41 -0300

    [Solved] Reverse Words Challenge

---
+++ ReverseWords/ReverseWords.py
@@ -0,0 +1,11 @@
+# File Handling
+with open('Inputs/B-large-practice.in', 'r') as f_reader:
+    with open('B-large-practice.out' ,'w') as f_writer:
+    
+        # Number of test cases
+        test_cases = int(f_reader.readline())
+        i = 1
+        while i <= test_cases:
+           fline = f_reader.readline().strip()
+           f_writer.write("Case #%d: %s\n" % (i, ' '.join(fline.split(' ')[::-1])))
+           i = i + 1
