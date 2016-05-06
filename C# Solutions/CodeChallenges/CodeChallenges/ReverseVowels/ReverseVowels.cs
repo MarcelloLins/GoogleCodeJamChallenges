c227bce58c7aa30f32801918293be71c83f87f1c
Marcello Lins <marcello.grechi@gmail.com>
2016-05-06 14:14:24 -0300

    [Change/Add] . New Challenges added + Added C# Answers of other challenges too

---
+++ C# Solutions/CodeChallenges/CodeChallenges/ReverseVowels/ReverseVowels.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenges
+{
+    class ReverseVowels
+    {
+         /**
+         *  Implementation: 
+         *   - Sets two pointers, at the end and at the start of the string
+         *   - Moves the left-most pointer to the right until it finds a vowel
+         *   - If a Vowel is found, moves the other pointer to the left, until a vowel is found
+          *  - Once both vowels are found, swaps them
+          * 
+          * 
+          * Notes: 
+          *     - An integer is kept to keep track whether the two vowels were found so the swap routine can trigger.
+          *     - C# Strings are immutable, hence, I had to work with Char[] instead to do the swapping of the vowels
+          *     - There's an extra routine I wrote to check whether a character is a Vowel or not.          * 
+         */
+
+
+        /// <summary>
+        /// Given the string received, it reverses each vowel of the string with the oposite one
+        /// on the word.
+        /// 
+        /// E.G: HelloL   => HolleL
+        ///      Hugs     => Hugs
+        ///      Marcello => Morcella
+        /// </summary>
+        /// <param name="s">The word</param>
+        /// <returns>Same string with the vowels reversed</returns>
+        public static string ReverseVowels (string s)
+        {
+            // Indexes / Pointers
+            int lowerIdx = 0;
+            int upperIdx = s.Count () - 1;
+
+            // Step Value (+1 to move the pointer from Left to Right, -1 to move the pointer from Right to Left)
+            int stepValue = 1;
+
+            // Basic Sanity check for extraordinary cases
+            if (String.IsNullOrWhiteSpace (s))
+            {
+                return String.Empty;
+            }
+
+            if (s.Count () == 1)
+            {
+                return s;
+            }
+
+            // Work-Variables
+            char lwrCharacter, upprCharacter, tmpVowel;
+            int vowelsFound = 0;
+
+            // Working with Char Arrays, that are mutable unlike strings in C#
+            var word = s.ToCharArray ();
+
+            // Stop Condition: While the pointers have not crossed yet
+            while (lowerIdx < upperIdx)
+            {
+                // Should we move the left most pointer forward ?
+                if (stepValue == 1)
+                {
+                    lwrCharacter = word[lowerIdx];
+
+                    if (IsVowel (lwrCharacter))
+                    {
+                        stepValue = -1;
+                        vowelsFound++;
+                    }
+                    else
+                    {
+                        lowerIdx += stepValue;
+                    }
+                }
+
+                // Should we move the right most pointer backward ?
+                if (stepValue == -1)
+                {
+                    upprCharacter = word[upperIdx];
+                    if (IsVowel (upprCharacter))
+                    {
+                        stepValue = 1;
+                        vowelsFound++;
+                    }
+                    else
+                    {
+                        upperIdx += stepValue;
+                    }
+                }
+
+                // Swaping Vowels
+                if (vowelsFound == 2)
+                {
+                    tmpVowel = word[lowerIdx];
+                    word[lowerIdx] = word[upperIdx];
+                    word[upperIdx] = tmpVowel;
+                    vowelsFound = 0;
+
+                    upperIdx--;
+                    lowerIdx++;
+                }
+            }
+
+            // Building string out of Char[]
+            return new string (word);
+        }
+
+        private static bool IsVowel (char c)
+        {
+            return new[] { 'a', 'e', 'i', 'o', 'u' }.Contains (char.ToLower (c));
+        }
+    }
+}
---
+++ C# Solutions/CodeChallenges/CodeChallenges/TopKFrequent Elements/TopKFrequentElements.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeChallenges.TopKFrequent_Elements
+{
+    class TopKFrequentElements
+    {
+        /**
+         *  Implementation: 
+         *   - Iterates through the array building a dictionary of frequencies of each found integer
+         *   - Sorts the Dictionary in Reverse order
+         *   - Take the first K elements of the sorted collection
+         */
+
+        /// <summary>
+        /// Returns the top K elements from the original array, sorted by their frequency in the array itself.
+        /// </summary>
+        /// <param name="nums">Array of numbers to be analized</param>
+        /// <param name="k">Slice Size</param>
+        /// <returns>List of the top "K" elements from the original array</returns>
+        public IList<int> TopKFrequent (int[] nums, int k)
+        {
+            Dictionary<int, int> frequencies = new Dictionary<int, int> ();
+
+            // Grouping Frequencies in a Dictionary - COST O(n)
+            foreach(int n in nums)
+            {
+                if (frequencies.ContainsKey(n))
+                {
+                    frequencies[n]++;
+                }
+                else
+                {
+                    frequencies.Add (n, 1);
+                }
+            }
+
+            // Sorting the Dictionary and Slicing the Top K - COST O(n Log(n)) + K = O(n Log(n))
+            return frequencies.OrderByDescending (t => t.Value).Select(t => t.Key).Take(k).ToList();
+        }
+    }
+}
